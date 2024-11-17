577388be0ae934fae4e2eada2c27991b95a86675
danielmenachem <daniellemenachem@gmail.com>
2024-11-17 02:59:51 +0200

    first version

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,21 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String name = args[0].toUpperCase(); 
+            int reps = Integer.parseInt(args[1]); 
+            String aOrAn = "a"; 
+            String anLetters = "AEFHILMNORSX"; 
+            for (int i = 0; i < name.length(); i++) {
+                if (anLetters.indexOf(name.charAt(i)) != -1){
+                        aOrAn = "an";
+                } else {
+                        aOrAn = "a";
+                }
+                System.out.println("Give me " + aOrAn + " " + name.charAt(i) + ": " + name.charAt(i) + "!");
+            }
+            System.out.println("What does it spell?");
+            for (int i = 0; i < reps; i++) {
+                System.out.println(name + "!!!");
+            }
         }
 }
